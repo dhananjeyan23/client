013c5cc8a8a7c901530b17b1c9ec5732ec450736
dhananjeyan23 <jeyan.mech7@gmail.com>
2024-07-27 23:46:27 +0530

    okk

--- src/org/emp1/EmployeeInfo1.java
+++ src/org/emp1/EmployeeInfo1.java
@@ -6,6 +6,7 @@ public class EmployeeInfo1 {
 	//data initialisation
 	int emp1 = 123456;
 	String ename = "psd";
+	String ename1="jps";
 	
 	//create the 1st method empId
 	private void empId() {
@@ -15,12 +16,17 @@ public class EmployeeInfo1 {
 	private void empName() {
 		System.out.println("employeename : "+ename);
 	}
+	//create the 3nd method empName
+		private void empName1() {
+			System.out.println("employeename : "+ename1);
+		}
 	//create the main method 
 	public static void main(String[] args) {
 		//classname objrefname = new classname()
 		EmployeeInfo1 ei1 = new EmployeeInfo1();
 		ei1.empId();
 		ei1.empName();
+		ei1.empName1();
 	}
 	
 	
